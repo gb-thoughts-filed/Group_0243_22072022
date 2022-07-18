87cc58fe52c01bb47ca21d229c5a2222bd639f2a
Gabrielle Browne <gabrielle.browne@mail.utoronto.ca>
2022-07-18 19:37:59 -0400

    gameboard canvas that's actually going to have the pictures updated, few lines to the original window

--- phase1/src/GUI.java
+++ phase1/src/GUI.java
@@ -6,10 +6,16 @@ public class GUI {
     public GUI() {
         JFrame window  = new JFrame("Not Flappy Bird");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(300, 600);
+        //window.setSize(300, 600);
+
+        gameboard_canvas canvas = new gameboard_canvas();
+
+        window.add(canvas);
 
         window.setLocationRelativeTo(null);
         window.setResizable(false);
+
+        window.pack();
         window.setVisible(true);
 
     }
---
+++ phase1/src/gameboard_canvas.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+// source for the inspiration/guidance for this code: https://www.youtube.com/watch?v=IyBsWymfqms
+
+public class gameboard_canvas extends JPanel {
+
+    public gameboard_canvas(){
+
+        setPreferredSize(new Dimension(300, 600));
+
+        setBackground(Color.white);
+
+
+    }
+
+}
