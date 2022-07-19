3e5edfff85b43ac5e9c0837d77f08c809fe75050
lzhur <liuba.zhurkevich@mail.utoronto.ca>
2022-07-19 11:10:57 -0400

    Added some javadoc to Leaderboard

--- phase1/src/Leaderboard.java
+++ phase1/src/Leaderboard.java
@@ -3,10 +3,16 @@ import java.util.*;
 public class Leaderboard {
     private Map<String, Integer> scoreMap;
 
+    /**
+     * Creates new Leaderboard object.
+     */
     public Leaderboard(){
         scoreMap = new HashMap<>();
     }
 
+    /**
+     *
+     */
     public void setScoreMap(){
         IntegerComparator n = new IntegerComparator(scoreMap);
         TreeMap<String, Integer> sorted = new TreeMap<String, Integer>(n);
@@ -19,11 +25,22 @@ public class Leaderboard {
         return sorted;
     }
 
+    /**
+     * Adds a new user and their highest score to the leaderboard.
+     * @param username username of the user
+     * @param score user's highest score
+     */
     public void addNewScore(String username, int score){
         scoreMap.put(username, score);
         new IntegerComparator(scoreMap);
     }
 
+    /**
+     * Updates the score of the user that is already on the leaderboard to a new highest score.
+     * @param username username of the user
+     * @param score new highest score of the user
+     * @return boolean
+     */
     public boolean updateExistingScore(String username, int score){
         if (!scoreMap.containsKey(username)){
             return false;
