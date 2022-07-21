5f01a70fad59ead17849bb102448bb46e611ba5a
jeanineohene-agyei <jeaninecoa@gmail.com>
2022-07-21 15:55:14 -0400

    changed Menu

--- phase1/src/GameProgram/Menu.java
+++ phase1/src/GameProgram/Menu.java
@@ -8,24 +8,24 @@ public class Menu {
         PlayGame play = new PlayGame(user);
         Scanner myObj = new Scanner(System.in);
         int line;
-        System.out.println("Welcome to our Game. Please select a menu option:");
-        System.out.println("1. PLAY GAME");
-        System.out.println("2. LEADERBOARD");
-        System.out.println("3. HOW TO PLAY");
 
-        String userInput = myObj.nextLine();
-        line = play.selectOption(userInput);
+        while (play.isRunning()) {
+            System.out.println("Welcome to our Game. Please select a menu option:");
+            System.out.println("1. PLAY GAME");
+            System.out.println("2. LEADERBOARD");
+            System.out.println("3. HOW TO PLAY");
 
-        if(line ==1) {
-            System.out.println("BEGIN GAME");
+            String userInput = myObj.nextLine();
+            line = play.selectOption(userInput);
+
+            if (line == 1) {
+                System.out.println("BEGIN GAME");
+            } else if (line == 2) {
+                System.out.println(play.displayLeaderboard());
+            } else if (line == 3) {
+                System.out.println("\"Use UP and DOWN arrow keys to move the bird and avoid obstacles. Collect golden apples \" +\n" +
+                        "                        \"to increase your score. Avoid poison apples or the game will end!\"");
+            }
         }
-        else if(line ==2) {
-            System.out.println(play.displayLeaderboard());
-        }
-        else if(line ==3){
-        System.out.println("\"Use UP and DOWN arrow keys to move the bird and avoid obstacles. Collect golden apples \" +\n" +
-                "                        \"to increase your score. Avoid poison apples or the game will end!\"");
-    }
-
 }
 }
