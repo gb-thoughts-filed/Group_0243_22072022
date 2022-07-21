afd620f25c65077ab5db7e550442c897863112e2
Gabrielle Browne <gabrielle.browne@mail.utoronto.ca>
2022-07-21 13:14:52 -0400

    made GameGUIControllerInterface

---
+++ phase1/src/GameProgram/GameGUIControllerInterface.java
@@ -0,0 +1,16 @@
+package GameProgram;
+
+public interface GameGUIControllerInterface {
+
+    public void makeGamePlayer();
+
+    public void makeObstacle();
+
+    public void makeGoldenApple();
+
+    public void makePoisonApple();
+
+    public void moveallleft();
+
+
+}
