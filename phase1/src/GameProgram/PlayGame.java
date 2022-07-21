d437b642f384a4d09d798979193ea298aed6b69a
jeanineohene-agyei <jeaninecoa@gmail.com>
2022-07-21 15:49:17 -0400

    changed Menu

--- phase1/src/GameProgram/PlayGame.java
+++ phase1/src/GameProgram/PlayGame.java
@@ -29,7 +29,7 @@ public class PlayGame {
     }
 
     public int selectOption(String input){
-        int output = 0;
+        int output;
         if (input.equals("1")){
             output = 1;
             runGame();
